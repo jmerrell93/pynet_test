32b08ec9e00213d7436abb11cf216f3fa5912c9a
Josh_Merrell <jmerrell93@hotmail.com>
2017-08-01 14:05:58 -0700

    edits to improve efficiency

--- telnet_conn.py
+++ telnet_conn.py
@@ -5,36 +5,33 @@ from snmp_helper import snmp_get_oid,snmp_extract
 
 TELNET_TIMEOUT = 6
 TELNET_PORT = 23
-#OID = '1.3.6.1.2.1.1.1.0'
-#ip_addr = ['184.105.247.70', '184.105.247.71']
 COMMUNITY = 'galileo'
 SNMP_PORT = 161
 username = 'pyclass'
 password = '88newclass'
+OID_NAME = dict()
+OID_NAME['1.3.6.1.2.1.1.1.0'] = 'DEVICE DESCRIPTION'
+OID_NAME['1.3.6.1.2.1.1.5.0'] = 'DEVICE NAME'
 
 def connect_and_query(ip_address):
     remote_conn = telnetlib.Telnet(ip_address, TELNET_PORT, TELNET_TIMEOUT)
     remote_conn.write(username + '\n')
-    time.sleep(2)
+    time.sleep(1)
     remote_conn.write(password + '\n')
     remote_conn.write("terminal length 0" + '\n')
-#    remote_conn.write("show ip int br" + '\n')
     time.sleep(1)
     output = remote_conn.read_very_eager()
     print output
-    remote_conn.close()
+    snmp_query('1.3.6.1.2.1.1.1.0')
+    snmp_query('1.3.6.1.2.1.1.5.0')
+
+def snmp_query(OID):
     device = (ip_address, COMMUNITY, SNMP_PORT)
-    OID = '1.3.6.1.2.1.1.1.0'
     snmp_get = snmp_get_oid(device, OID)
     output = snmp_extract(snmp_get)
-    print "FOR DEVICE %s" %ip_address
-    print "DEVICE DESCRIPTION"
+    print "FOR DEVICE %S" %ip_address
+    print OID_NAME[OID]
     print output + '\n'
-    OID = '1.3.6.1.2.1.1.5.0'
-    snmp_get = snmp_get_oid(device, OID)
-    output = snmp_extract(snmp_get)
-    print "DEVICE NAME"
-    print output
 
 connect_and_query('184.105.247.70')
 connect_and_query('184.105.247.71')
