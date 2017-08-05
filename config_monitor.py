ef0ffba354f2929daefd70353c9c377ce7a6bc56
Josh_Merrell <jmerrell93@hotmail.com>
2017-08-04 17:57:14 -0700

    updates

--- config_monitor.py
+++ config_monitor.py
@@ -19,13 +19,12 @@ OID_NAME['1.3.6.1.2.1.1.1.0'] = 'DEVICE DESCRIPTION'
 OID_NAME['1.3.6.1.2.1.1.5.0'] = 'DEVICE NAME'
 OID_NAME['1.3.6.1.4.1.9.9.43.1.1.1.0'] = 'UPTIME WHEN CONFIG LAST CHANGED'
 
-recipient = 'jmerrell93@hotmail.com'
+recipient = 'michael@methridge.com'
 sender = 'josh.merrell@wwt.com'
 subject = 'Notice of Configuration Change'
 
 def send_email(recipient, subject, message, sender):
     email_helper.send_mail(recipient, subject, message, sender)
-
 
 def config_poll_1(ip_address):
     remote_conn = telnetlib.Telnet(ip_address, TELNET_PORT, TELNET_TIMEOUT)
@@ -61,14 +60,14 @@ def snmp_query(OID, ip_address):
     output = snmp_extract(snmp_get)
     print "\nFOR DEVICE %s" %ip_address
     print OID_NAME[OID]
-    print output + '\n'
+    return output
 
 info_1 = snmp_query('1.3.6.1.2.1.1.5.0', '184.105.247.71')
 info_2 = snmp_query('1.3.6.1.4.1.9.9.43.1.1.1.0', '184.105.247.71')
-message = '%s' %info_1 + '%s' %info_2
+message = 'This is an awesome and somewhat hokey automated alert informing you that the following device has undergone recent configuration change\n\n- - - - - - - - -\nDEVICE NAME\n%s\n\nUPTIME AT TIME OF LAST CONFIGURATION CHANGE\n%s\n\nThe Source Code for this script can be found at https://github.com/jmerrell93/pynet_work/blob/master/config_monitor.py :D' %(info_1, info_2)
 
 config_poll_1('184.105.247.71')
-time.sleep(480)
+time.sleep(660)
 config_poll_2('184.105.247.71')
 f = open('poll_1.py', 'rb')
 a = pickle.load(f)
@@ -79,9 +78,5 @@ b
 if filecmp.cmp('poll_1.py', 'poll_2.py'):
     print "no changes"
 else:
-#    print "This is an alert that the following device has changed:\n"
-#    print "- - - - - - - - - - - - - - - - - - - - - - - - - - - - -"
-#    snmp_query('1.3.6.1.2.1.1.5.0', '184.105.247.71')
-#    snmp_query('1.3.6.1.4.1.9.9.43.1.1.1.0', '184.105.247.71')
     send_email(recipient, subject, message, sender)
-
+    print "Email Sent"
